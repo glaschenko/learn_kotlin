8af8012f606e3f3763ea3069d26ec488f0818270
glaschenko <a.glaschenko@gmail.com>
2017-12-10 22:13:46 +0400

    first

---
+++ src/learn/kotlin/Main.kt
@@ -0,0 +1,16 @@
+package learn.kotlin
+
+
+/*
+ * Author: glaschenko
+ * Created: 10.12.2017
+ */
+fun main(args: Array<String>){
+    print("Sum of 3 and 5 is " + sum(3,5));
+}
+
+fun sum(a: Int, b: Int): Int{
+    return a+b;
+}
+
+
